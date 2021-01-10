da61bd6ac00d227b1f476d200b3df1c35f871f75
Mr. Lan <fdevops@163.com>
2021-01-10 21:57:37 +0800

    feat: add menu file.

---
+++ src/views/cmdb/business/host-apply.vue
@@ -0,0 +1,23 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/cmdb/business/service-template.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    服务模板
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
