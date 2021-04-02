1a59a16b57f7552bae3660d92d02f8f2d86afbc7
lanyulei <fdevops@163.com>
2021-04-03 00:13:37 +0800

    feat: 调整首页展示。

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,25 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    欢迎访问 fiy 系统
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
-
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
   data() {
     return {
-      currentRole: 'adminDashboard'
     }
   },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
-  },
   created() {
-    // if (!this.roles.includes('admin')) {
-    //   this.currentRole = 'editorDashboard'
-    // }
   }
 }
 </script>
+
+<style scoped>
+  .dashboard-container {
+    margin-top: 150px;
+    font-size: 50px;
+    text-align: center;
+  }
+</style>
