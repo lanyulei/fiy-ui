d1c23897f720c7e21abb246da984d6ff505e3946
lanyulei <fdevops@163.com>
2021-04-20 00:26:24 +0800

    feat: 添加k8s集群页面。

---
+++ src/views/k8s/project/cluster/info.vue
@@ -0,0 +1,34 @@
+<template>
+  <BasicLayout>
+    <template #wrapper>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>集群详情</span>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{ '列表内容 ' + o }}
+        </div>
+      </el-card>
+    </template>
+  </BasicLayout>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
